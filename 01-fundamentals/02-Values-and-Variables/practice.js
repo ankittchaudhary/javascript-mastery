161e24a733f3310f363a539c48e2e9449f6f6744
Ankit Personal <chaudharyankit2022@gmail.com>
2026-08-08 00:07:50 +0530

    feat: values and variables practice add

---
+++ 01-fundamentals/02-Values-and-Variables/practice.js
@@ -0,0 +1,13 @@
+//Q.1 Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+// Log their values to the console.
+
+
+// Declare variables
+let country = "India";
+let continent = "Asia";
+let population = "1.47 Billion"
+
+//log values
+console.log(country);
+console.log(continent);
+console.log(population);
